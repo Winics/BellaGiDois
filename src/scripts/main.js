9ddfb2248fa35adfd72424083c6bd1deaaabe259
Winics <winics.g@gmail.com>
2025-06-13 00:21:57 -0300

    comitão

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
             let produto = botao.closest(".produto"); // Encontra o contêiner do produto
             let modelo = produto.getAttribute("data-modelo"); // Obtém o modelo do produto
             let preco = produto.querySelector("p").innerText.replace("Preço: ", ""); // Obtém o preço
-            imagemUrl = "https://bella-gi-dois-oer927zq0-winics-projects.vercel.app/images/catalogo/calcinha1.jpg"; // Link da imagem
+            imagemUrl = "https://bella-gi-dois.vercel.app/images/catalogo/calcinha1.jpg"; // Link da imagem
 
             let mensagem = `Olá! Estou interessado no modelo: ${modelo}, que custa ${preco}. Veja a imagem aqui: ${imagemUrl}`;
             let numero = "41991902924"; // Seu número com DDD
